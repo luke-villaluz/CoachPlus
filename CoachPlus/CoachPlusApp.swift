21667ca25efee2ec71369eee26a4b612918d168b
Luke <luke.villaluz@gmail.com>
2024-03-14 00:38:02 -0700

    Initial Commit

---
+++ CoachPlus/CoachPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoachPlusApp.swift
+//  CoachPlus
+//
+//  Created by Luke Villaluz on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct CoachPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
